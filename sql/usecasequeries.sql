7b402a7f897f349d2ee9488df70a1ffb14f72472
Nageshwara sairam <113401242+nagesh161007@users.noreply.github.com>
2022-11-12 22:27:05 -0500

    refactor queries and add relational algebra

--- sql/usecasequeries.sql
+++ sql/usecasequeries.sql
@@ -1,51 +1,60 @@
 -- 1.What user posted this tweet?
 
-SELECT tu.name,tu.twitter_user_id, t.tweet_text
-FROM Tweets AS t, TwitterUser AS tu
-WHERE t.tweet_id='1590933806558883840' AND t.tweet_author_id=tu.twitter_user_id;
+SELECT tu.name, tu.twitter_user_id, t.tweet_text
+FROM Tweets AS t INNER JOIN TwitterUser AS tu
+on t.tweet_author_id=tu.twitter_user_id WHERE t.tweet_id='1590933806558883840';
 
+-- Π TwitterUser.name tweet_id = "1590933806558883840" (Tweets ⋈ Tweets.tweet_author_id  =  TwitterUser.twitter_user_id (TwitterUser))
 
 -- 2.When did the user post this tweet?
 
-SELECT tu.name,tu.twitter_user_id, t.created_at
-FROM Tweets AS t, TwitterUser AS tu
-WHERE t.tweet_id='1590933806558883840' AND t.tweet_author_id=tu.twitter_user_id;
+SELECT t.created_at
+FROM Tweets AS t INNER JOIN TwitterUser AS tu
+ON t.tweet_author_id=tu.twitter_user_id WHERE t.tweet_id='1590933806558883840';
 
+-- Π Tweets.created_at tweet_id = "1590933806558883840" (Tweets ⋈ Tweets.tweet_author_id  =  TwitterUser.twitter_user_id (TwitterUser))
 
 -- 3.What tweets have this user posted in the past 24 hours?
 
-SELECT tu.name, tu.twitter_user_id, tu.created_at, t.tweet_text
-FROM Tweets AS t, TwitterUser AS tu
-WHERE t.tweet_author_id='1589634110984699904' AND t.tweet_author_id=tu.twitter_user_id AND t.created_at >= NOW() - INTERVAL 2 DAY;
+SELECT  * FROM Tweets AS t INNER JOIN TwitterUser AS tu ON t.tweet_author_id=tu.twitter_user_id
+WHERE t.tweet_author_id='1589634110984699904' AND t.created_at >= NOW() - INTERVAL 1 DAY;
+
+-- σ tweet_id = "1590933806558883840 and created_at > NOW() - INTERVAL 1 DAY (Tweets ⋈ Tweets.tweet_author_id  =  TwitterUser.twitter_user_id (TwitterUser))
 
 -- 4.How many tweets have this user posted in the past 24 hours?
 
 SELECT count(t.tweet_id)
-FROM Tweets AS t, TwitterUser AS tu
-WHERE t.tweet_author_id='1589634110984699904' AND t.tweet_author_id=tu.twitter_user_id AND t.created_at >= NOW() - INTERVAL 2 DAY;
+FROM Tweets AS t INNER JOIN TwitterUser AS tu on t.tweet_author_id=tu.twitter_user_id
+WHERE t.tweet_author_id='1589634110984699904' AND t.created_at >= NOW() - INTERVAL 1 DAY;
+
+-- σ COUNT(*)tweet_id = "1590933806558883840 and created_at > NOW() - INTERVAL 1 DAY (Tweets ⋈ Tweets.tweet_author_id  =  TwitterUser.twitter_user_id (TwitterUser))
 
 
 -- 5.When did this user join Twitter?
 
-SELECT tu.name,tu.twitter_user_id, tu.created_at
-FROM Tweets AS t, TwitterUser AS tu
-WHERE t.tweet_id='1590933806558883840' AND t.tweet_author_id=tu.twitter_user_id;
+SELECT tu.created_at
+FROM Tweets AS t INNER JOIN TwitterUser AS tu on t.tweet_author_id=tu.twitter_user_id
+WHERE t.tweet_id='1590933806558883840';
+
+-- Π TwitterUser.created_at tweet_id = "1590933806558883840" (Tweets ⋈ Tweets.tweet_author_id  =  TwitterUser.twitter_user_id (TwitterUser))
 
 -- 6.What keywords/ hashtags are popular?
 
-SELECT DISTINCT hash_tag, count(hash_tag)
+SELECT DISTINCT hash_tag
 FROM Hashtags
 GROUP BY hash_tag
 ORDER BY count(hash_tag) DESC
 LIMIT 2;
 
+-- τ -COUNT(hash_tag) (Hashtags) γ hash_tag
 
 -- 7.What tweets are popular?
 
-SELECT t.tweet_text, t.tweet_id, m.like_count
+SELECT t.tweet_text
 FROM Tweets AS t
 INNER JOIN Metrics as m on t.tweet_id = m.tweet_id ORDER BY m.like_count DESC;
 
+-- τ -Metrics.like_count Π Tweets.tweet_text  (Tweets ⋈ Tweets.tweet_id  =  Metrics.tweet_id (Metrics))
 
 
 
