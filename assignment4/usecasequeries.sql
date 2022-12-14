d735ac5e870a628c2a3dd71bc66908edae4cd5ec
AKSHITHA KAMSHETTY <113708839+akshithakamsy@users.noreply.github.com>
2022-12-13 23:40:58 -0500

    Update usecasequeries.sql

--- assignment4/usecasequeries.sql
+++ assignment4/usecasequeries.sql
@@ -54,21 +54,28 @@ select * from decemberseason_vacacies;
 -- select * from permanentaccommodation where gender = 'GIRL' and rent < 600 and start BETWEEN '2022-12-15' AND '2022-12-31' and roomtype = 'SHARED'
 
 -- 6. List names the users who posted temporary accomodation for female 
+CREATE VIEW girl _temp_acc as 
+select usr.name from user usr inner join temporaryaccommodation tmp on usr.userid = tmp.userid where tmp.gender='GIRL';
 
-select usr.name from user usr inner join temporaryaccommodation tmp on usr.contact = tmp.number where tmp.gender='GIRL';
+--select usr.name from user usr inner join temporaryaccommodation tmp on usr.contact = tmp.number where tmp.gender='GIRL';
 
 -- 7. List contact of user from permanent accommodations with rent greater than 500
+CREATE VIEW per_acc_rent500 as 
+select pac.contact, usr.name from user usr inner join permanentaccommodation pac on usr.userid = pac.userid where pac.rent > 500;
 
-select pac.contact, usr.name from user usr inner join permanentaccommodation pac on usr.contact = pac.contact where pac.rent > 500;
+--select pac.contact, usr.name from user usr inner join permanentaccommodation pac on usr.contact = pac.contact where pac.rent > 500;
 
 
 -- 8. List the id with contact no in permanent accommodation with start date after December
-select pac.contact, pac.address from user usr inner join permanentaccommodation pac on usr.contact = pac.contact where pac.start >2022-12-31;
+CREATE VIEW per_acc_startdate as select pac.contact, pac.address from user usr inner join permanentaccommodation pac on usr.userid = pac.userid where pac.start >2022-12-31
+--select pac.contact, pac.address from user usr inner join permanentaccommodation pac on usr.contact = pac.contact where pac.start >2022-12-31;
 
 
 -- 9. List the names in permanent accommodation with 1 bed type
-
-select pac.contact, pac.address from user usr inner join permanentaccommodation pac on usr.contact = pac.contact where pac.bed=1;
+CREATE VIEW per_acc_1bed as 
+select pac.contact, pac.address from user usr inner join permanentaccommodation pac on usr.userid = pac.userid where pac.bed=1;
+--select pac.contact, pac.address from user usr inner join permanentaccommodation pac on usr.contact = pac.contact where pac.bed=1;
 
 -- 10. List the names of the temporary accommodation with descriptions
-select usr.name from user usr inner join temporaryaccommodation tmp on usr.contact = tmp.number where tmp.description is not null;
+CREATE VIEW temp_acc_desc as select usr.name from user usr inner join temporaryaccommodation tmp on usr.userid = tmp.userid where tmp.description is not null;
+--select usr.name from user usr inner join temporaryaccommodation tmp on usr.contact = tmp.number where tmp.description is not null;
